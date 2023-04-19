659b5ada5d10d4567155480cf6d838ad868350ef
HoaAyWK <vycoi1511@gmail.com>
2023-04-19 22:23:49 +0700

    Update create product origin

--- client-app/src/features/admin/components/index.js
+++ client-app/src/features/admin/components/index.js
@@ -1,4 +1,4 @@
 export { default as DataTable } from './DataTable';
-export { default as FetchDateErrorMessage } from './FetchDataErrorMessage';
+export { default as FetchDataErrorMessage } from './FetchDataErrorMessage';
 export { default as Loading } from './Loading';
 export { default as LabelAndContent } from './LabelAndContent';
--- client-app/src/features/admin/product-origin/ProductOriginForm.jsx
+++ client-app/src/features/admin/product-origin/ProductOriginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -9,36 +9,31 @@ import { useSnackbar } from 'notistack';
 import { useDispatch, useSelector } from 'react-redux';
 
 import ACTION_STATUS from '../../../constants/actionStatus';
-import { FormProvider, RHFEditor, RHFSelect, RHFTextField } from '../../../components/hook-form';
+import { FormProvider, RHFEditor, RHFSelect, RHFMultiSelect, RHFTextField } from '../../../components/hook-form';
 import { refresh } from './productOriginSlice';
 import { getCategories, selectAllCategories } from '../category/categorySlice';
-import { Loading } from '../../../components';
-
-const brands = [
-  {
-    id: 1,
-    name: 'Samsung'
-  },
-  {
-    id: 2,
-    name: 'Apple'
-  },
-  {
-    id: 3,
-    name: 'Lenovo'
-  }
-];
+import { FetchDataErrorMessage, Loading } from '../components';
+import { getBrands, selectAllBrands } from '../brand/brandSlice';
 
 
 const ProductOriginForm = ({ isEdit, product, action, status }) => {
   const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
   const categories = useSelector(selectAllCategories);
+  const brands = useSelector(selectAllBrands);
   const { getCategoriesStatus } = useSelector((state) => state.adminCategories);
+  const { getBrandsStatus } = useSelector((state) => state.adminBrands);
+  const [categoryItems, setCategoryItems] = useState([]);
+  const [initialDescription, setInitialDescription] = useState('<p>Hello world</p>\n');
+  const [initialInformation, setInitialInformation] = useState('<p>Information</p>\n');
 
   useEffect(() => {
     if (getCategoriesStatus === ACTION_STATUS.IDLE) {
       dispatch(getCategories());
+    }
+
+    if (getBrandsStatus === ACTION_STATUS.IDLE) {
+      dispatch(getBrands());
     }
   }, []);
 
@@ -49,16 +44,19 @@ const ProductOriginForm = ({ isEdit, product, action, status }) => {
       .required('Description is required'),
     information: Yup.string()
       .required('Specification is required'),
-    category: Yup.string(),
-    brand: Yup.string(),
+    category: Yup.array()
+      .required('Category is required')
+      .min(1, 'Category is required'),
+    brand: Yup.string()
+      .required('Brand is required'),
   });
 
   const defaultValues = product ? product : {
     name: '',
     description: '',
     information: '',
-    category: categories?.[0]?.id,
-    brand: brands[0].id,
+    category: [],
+    brand: brands?.[0]?.id,
   };
 
   const methods = useForm({
@@ -68,8 +66,11 @@ const ProductOriginForm = ({ isEdit, product, action, status }) => {
 
   const { handleSubmit, reset } = methods;
 
+  const handleCategoryItemsChange = (items) => {
+    setCategoryItems(items);
+  };
+
   const onSubmit = async (data) => {
-    console.log(data);
     try {
       const actionResult = await dispatch(action(data));
       const result = unwrapResult(actionResult);
@@ -78,14 +79,24 @@ const ProductOriginForm = ({ isEdit, product, action, status }) => {
         enqueueSnackbar(`${isEdit ? 'Updated' : 'Created'} successfully`, { variant: 'success' });
         reset();
         dispatch(refresh());
+        setCategoryItems([]);
+        setInitialDescription('<p>Hello world</p>\n');
+        setInitialInformation('<p>Information</p>\n');
       }
     } catch (error) {
       enqueueSnackbar(error.message, { variant: 'error' });
     }
   };
 
-  if (getCategoriesStatus === ACTION_STATUS.LOADING) {
+  if (getCategoriesStatus === ACTION_STATUS.LOADING ||
+      getCategoriesStatus === ACTION_STATUS.IDLE ||
+      getBrandsStatus === ACTION_STATUS.LOADING ||
+      getBrandsStatus === ACTION_STATUS.IDLE) {
     return <Loading />;
+  }
+
+  if (getCategories === ACTION_STATUS.FAILED || getBrandsStatus === ACTION_STATUS.FAILED) {
+    return <FetchDataErrorMessage />;
   }
 
   return (
@@ -96,11 +107,38 @@ const ProductOriginForm = ({ isEdit, product, action, status }) => {
             <CardContent>
               <Stack spacing={2}>
                 <RHFTextField name='name' label='Name' />
-                <RHFEditor name='description' label='Description' initialContent={'<p>Hello world</p>\n'} />
-                <RHFEditor name='information' label='Information' initialContent={''} />
+                <RHFEditor name='description' label='Description' initialContent={initialDescription} />
+                <RHFEditor name='information' label='Information' initialContent={initialInformation} />
               </Stack>
             </CardContent>
           </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ borderRadius: 1 }}>
+            <CardContent>
+              <Stack spacing={2}>
+                <RHFMultiSelect
+                  name='category'
+                  data={categories}
+                  id='category'
+                  label='Category'
+                  items={categoryItems}
+                  onItemsChange={handleCategoryItemsChange}
+                  defaultValue={[]}
+                />
+                <RHFSelect
+                  name='brand'
+                  data={brands}
+                  id='brand'
+                  label='Brand'
+                />
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
           <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
             <LoadingButton
               type='submit'
@@ -112,26 +150,6 @@ const ProductOriginForm = ({ isEdit, product, action, status }) => {
               {`${isEdit ? 'Update' : 'Create'} product origin`}
             </LoadingButton>
           </Box>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card sx={{ borderRadius: 1 }}>
-            <CardContent>
-              <Stack spacing={2}>
-                <RHFSelect
-                  name='category'
-                  data={categories}
-                  id='category'
-                  label='Category'
-                />
-                <RHFSelect
-                  name='brand'
-                  data={brands}
-                  id='brand'
-                  label='Brand'
-                />
-              </Stack>
-            </CardContent>
-          </Card>
         </Grid>
       </Grid>
     </FormProvider>
